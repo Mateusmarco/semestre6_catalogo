9b175ee21d0a5edfd76d55c9d3b30d1be723a665
Mateusmarco <mateusmarcondesfontana@gmail.com>
2026-08-06 23:02:19 -0400

    feat: adiciona a classe category

---
+++ back-end/src/main/java/com/tads20262/catalago/entity/Category.java
@@ -0,0 +1,19 @@
+package com.tads20262.catalago.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@AllArgsConstructor
+@Data
+@NoArgsConstructor
+
+@Entity
+public class Category
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+
+
+}
